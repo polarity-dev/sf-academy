241aa23aa49e530d09dc0b9370350ca47a831838
Mattia Ferrari <mattia2002.ferrari@gmail.com>
2021-06-09 19:32:23 +0200

    added router in web service

--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,31 @@
-
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login"
+import Transactions from "./pages/Transactions";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/history">
+          <Transactions />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/signup">
+          <SignUp />
+        </Route>
+
+        <Route path="*">
+          <div>Not found</div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
