a3b18343dfcfadf155664daf8edf084d364c5b78
stefagr <grisendistefano@gmail.com>
2021-07-17 19:28:20 +0200

    test nuove funzioni

--- prove/exchange-microservice/users_test.js
+++ prove/exchange-microservice/users_test.js
@@ -16,6 +16,21 @@ client.Login({email: 'grisendistefano@gmail.com', password: 'password'}, (err, d
 });
 
 client.Deposit({token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJpYXQiOjE2MjYzNTU2MzZ9.ARoD0Q_TnpYqO7INo1ig9bOQtw9XThyQoCJoChXxt9c',
-      amount: 800.5, currency:'USD'}, (err, data) => {
+      amount: 800.5, currency:'EUR'}, (err, data) => {
   console.log("DEPOSIT: " , {err, data });
+});
+
+client.Withdraw({token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJpYXQiOjE2MjYzNTU2MzZ9.ARoD0Q_TnpYqO7INo1ig9bOQtw9XThyQoCJoChXxt9c',
+      amount: 400, currency:'EUR'}, (err, data) => {
+  console.log("WITHDRAW: " , {err, data });
+});
+
+client.Buy({token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJpYXQiOjE2MjYzNTU2MzZ9.ARoD0Q_TnpYqO7INo1ig9bOQtw9XThyQoCJoChXxt9c',
+      amount: 200, currencyFrom:'EUR', currencyTo:'JPY'}, (err, data) => {
+  console.log("BUY: " , {err, data });
+});
+
+client.ListTransactions({token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJpYXQiOjE2MjYzNTU2MzZ9.ARoD0Q_TnpYqO7INo1ig9bOQtw9XThyQoCJoChXxt9c',
+      filter: 0}, (err, data) => {
+  console.log("LIST TRANSACTIONS: " , {err, data });
 });
