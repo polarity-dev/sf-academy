c3792d4ecb458dd439aadda387ed776736b7630a
onemoredavide <davideantipa@gmail.com>
2022-03-04 15:09:57 +0100

    mongo uri, .dockerignore config

--- prova/src/config/db.ts
+++ prova/src/config/db.ts
@@ -1,10 +1,10 @@
 import * as mongoDB from "mongodb";
 
 // docker-compose up -d ---> DOCKER CONTAINER URI BELOW
-//const URI="mongodb://root:rootpassword@mongodb:27017";
+const URI="mongodb://root:rootpassword@mongodb:27017";
 
 //LOCALHOST MONGO URI
-const URI="mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+//const URI="mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 const client = new mongoDB.MongoClient(URI);
 client.connect();
 
