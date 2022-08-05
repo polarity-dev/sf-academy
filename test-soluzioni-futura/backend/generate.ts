00d6fdfc87b20669bed7242fe66dc50b8d272526
rickytrevor <rick@rickyscloud.com>
2022-08-05 12:05:45 +0200

    fix

--- test-soluzioni-futura/backend/generate.ts
+++ test-soluzioni-futura/backend/generate.ts
@@ -12,24 +12,24 @@ const lorem = new LoremIpsum({
     min: 4
   }
 });
-let n = Math.floor(Math.random() * 50) + 1;
 
-let a = Math.floor(Math.random() * n) + 1; 
-let b = Math.floor(Math.random() * n) + 1;
+
+var n = Math.floor(Math.random() * 50) + 1;
+var a = Math.floor(Math.random() * n) + 1; 
+var b = Math.floor(Math.random() * n) + 1;
 if(a > b){
-    let temp = a;
+    var temp = a;
     a = b;
     b = temp;
 }
 
 
-let str ="";
+var str ="";
 str += a + " " + b + "\n";
-for(let i = 0; i < n; i++){
-    let p = Math.floor(Math.random() * 5) + 1;
-    let m = Math.floor(Math.random() * 900) -450 ;
+for(var i = 0; i < n; i++){
+    var p = Math.floor(Math.random() * 5) + 1;
+    var m = Math.floor(Math.random() * 900) -450 ;
     str += p + " " + m + " " + lorem.generateSentences(1) + "\n";
-//    str += p + " " + m + "\n";
 }
 
 
