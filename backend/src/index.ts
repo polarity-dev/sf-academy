96afb8273cc98df053653ffe4a8d947090103c13
kanopo <dmitri.ollari@protonmail.com>
2023-01-05 18:38:00 +0100

    Express uploading file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,27 +1,51 @@
-import express, {Request, Response} from "express"
+import express, { Request, Response } from "express";
+import fileUploud from "express-fileupload";
+import path from "path";
 
+const port = process.env.PORT || 3000;
 const app = express()
 
+let fileCounter = 0;
+
 app.use(express.json())
+
+app.use(fileUploud())
+
 
 app.get("/pendingData", (req: Request, res: Response) => {
   return res.send("Hello world!")
 })
 
 app.get("/data", (req: Request, res: Response) => {
-  return res.send("Hello world!!!")
+  return res.send("ciao")
 })
 
 
-app.post("/importDataFromFile", (req: Request, res: Response) => {
-  console.log(req.body)
+app.post("/importDataFromFile", (req, res) => {
+  if (!req.files) {
+    return res.status(400).send("No files were uploaded.");
+  }
 
+  const file = req.files.data;
+
+  const path = __dirname + "/files/" + `${fileCounter}_` + file.name;
+
+  file.mv(path, (err) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+  })
+
+  fileCounter += 1
   return res.sendStatus(200)
+
+
 })
 
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000")
+app.listen(port, () => {
+  console.log("Server running on http://localhost: " + port)
 })
 
 
