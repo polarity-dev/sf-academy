482212271a74033b9bfbb1fcc7042fae5aefdf99
Dennis Supryn <116674938+SupDennis@users.noreply.github.com>
2023-01-12 11:28:01 +0100

    Create messaggi.sql

--- DB/messaggi.sql
+++ DB/messaggi.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS messaggi (
   "id" int(11) NOT NULL AUTO_INCREMENT,
   "K" smallint(6) NOT NULL,
   "D" varchar(5) NOT NULL,
-  "timestamp" timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  "timestamp" timestamp NOT NULL,
   PRIMARY KEY ("id")
 );
