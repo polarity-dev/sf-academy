497d7b4301b96172a0c3c40d6dc089b1e27c74a0
Alberto Mordonini <albmord@gmail.com>
2023-06-02 17:46:16 +0200

    Update logout.ts
    Rimosso contenuto della risposta

--- exchange-microservice-mordonini/packages/api/src/routes/logout.ts
+++ exchange-microservice-mordonini/packages/api/src/routes/logout.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 
 export const logoutUser = (req: Request, resp: Response) => {
-    resp.status(200).send("Bellaaaa")
+    resp.status(200).send("")
 }
 
