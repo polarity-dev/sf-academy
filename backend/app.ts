f14e6b345e06c6994163938f7d8584331a9a59b3
Matteo Angelini <matteo.angelini@outlook.com>
2023-08-30 22:05:47 +0200

    Add pgadmin to compose

--- backend/app.ts
+++ backend/app.ts
@@ -1,6 +1,10 @@
-import express from 'express';
-const app = express();
+import express, { Application } from 'express';
+const app: Application = express();
 const port = 3000;
+
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
