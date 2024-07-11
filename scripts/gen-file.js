46a64d852df07c5c1749551919ae3748a20b7dc1
ale18V <alebombarda@icloud.com>
2024-07-11 13:12:47 +0200

    Add utility script for generating data file

---
+++ scripts/gen-file.js
@@ -0,0 +1,18 @@
+const { argv } = require('process');
+const { LoremIpsum } = require('lorem-ipsum');
+const { randomInt } = require('crypto');
+const fs = require('fs');
+const path = require('path');
+
+const loremipsum = new LoremIpsum({});
+const N = Number(argv[2] ?? 50);
+const outFile = argv[3] ?? path.join(__dirname, "..", "data", "sample.txt");
+let buffer = "";
+
+loremipsum.generateWords(N).split(' ').forEach((word) => {
+    buffer += loremipsum.generator.generateRandomInteger(0, 5) + " " + word + "\n";
+});
+
+fs.writeFile(outFile, buffer, (err) => {
+    if (err) console.error(err);
+});
