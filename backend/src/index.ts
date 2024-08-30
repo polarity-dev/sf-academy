1194328965905f478ccd7d3d2467f1e2966992be
papera_matta <dalpone2005@gmail.com>
2024-08-30 19:32:21 +0200

    Updated index.ts to test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,33 +1,13 @@
 import Fastify from "fastify";
 import { initializeSSEManager } from "./sse/sseInitializer";
-import { handleNewConnection } from "./sse/connectionHandler";
-import { broadcastData } from "./sse/dataBroadcaster";
-import { delay } from "./utils/delayManager";
 import path from "path";
-import fs, { readFileSync } from "fs";
-import { Client } from "pg";
+import { readFileSync } from "fs";
+import { initCryptoEndpoints } from "./controllers/cryptoController";
+import db from "./database/dbInit";
 
 async function setup() {
 
     const { ADDRESS = 'localhost', PORT = '3000' } = process.env;
-
-    const client = new Client({
-        user: 'myuser',
-        host: 'db',
-        database: 'mydatabase',
-        password: 'mypassword',
-        port: 5432
-    });
-
-    try {
-   
-        await client.connect();
-
-        console.log((await client.query(`select * from cryptos;`)).rows);     
-    } catch (err) {
-        console.log(err);
-    }
-
 
     const server = Fastify({logger: true});
     
@@ -39,29 +19,9 @@ async function setup() {
         const htmlContent = readFileSync(htmlPath,"utf-8");
         reply.type("text/html").send(htmlContent);
     });
-
-    // test
-    server.get("/api/test", async function (request,reply) {
-        
-        const signal = await handleNewConnection(SSEManager,request,reply,"api/test");
-
-        var count = 0;
-
-        while (!signal.aborted) {
-            try {
-                broadcastData(SSEManager,"api/test",`sending for the ${count++} time`);
-                await delay(1000);
-            } catch (error) {
-                if (signal.aborted) {
-                    console.log("Loop terminated due to abort signal");
-                } else {
-                    console.log(error);
-                }
-            } 
-        }
-
-    });
     
+    await initCryptoEndpoints(SSEManager,server,db);
+
     server.listen({ host: ADDRESS, port: parseInt(PORT, 10) }, (err,address) => {
         if (err) {
             server.log.error(err);
