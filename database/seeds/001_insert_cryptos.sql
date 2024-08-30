7760519a08ff7ce96c54b00a7db6933a507b248e
papera_matta <dalpone2005@gmail.com>
2024-08-30 18:05:41 +0200

    Added seeds dir for database

---
+++ database/seeds/001_insert_cryptos.sql
@@ -0,0 +1,6 @@
+insert into cryptos (name,symbol,price) values 
+    ("Bitcoin","BTC",randomInRange(1,10000)),
+    ("BNB","BNB",randomInRange(1,10000)),
+    ("Ethereum","ETH",randomInRange(1,10000)),
+    ("Solana","SOL",randomInRange(1,10000)),
+    ("Tether","USDT",randomInRange(1,10000));
---
+++ database/seeds/002_insert_budget.sql
@@ -0,0 +1,1 @@
+insert into budget values (100000);
