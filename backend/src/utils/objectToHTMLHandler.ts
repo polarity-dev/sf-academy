cbc16fa35ee9b3608108b7b61273dcf981796474
papera_matta <dalpone2005@gmail.com>
2024-09-04 19:15:41 +0200

    Added colors for success / failed / pending state in transactions

--- backend/src/utils/objectToHTMLHandler.ts
+++ backend/src/utils/objectToHTMLHandler.ts
@@ -4,7 +4,7 @@ import transaction from "../models/transactionModel";
 import { getTable } from "../database/dbQueries";
 
 function tdMaker(value: string,cssClass: string = "") {
-    return `<td ${cssClass}>${value}</td>`;
+    return `<td class="${cssClass}">${value}</td>`;
 }
 
 export async function getCryptoHtml(db: Client) {
@@ -14,10 +14,10 @@ export async function getCryptoHtml(db: Client) {
         var htmlRes = ``;
         for (const crypto of cryptos) {
             htmlRes += "<tr>";
-            htmlRes += tdMaker(crypto.name.toString());
-            htmlRes += tdMaker(crypto.symbol.toString());
-            htmlRes += tdMaker(crypto.price.toString());
-            htmlRes += tdMaker(crypto.owned.toString());
+            htmlRes += tdMaker(crypto.name.toString(),"text-center");
+            htmlRes += tdMaker(crypto.symbol.toString(),"text-center");
+            htmlRes += tdMaker(crypto.price.toString(),"text-center");
+            htmlRes += tdMaker(crypto.owned.toString(),"text-center");
             htmlRes += "</tr>";
         }
         return { success: true, data: htmlRes }; 
@@ -33,11 +33,18 @@ export async function getTransactionHtml(db: Client) {
         var htmlRes = ``;
         for (const transaction of transactions) {
             htmlRes += "<tr>";
-            htmlRes += tdMaker(transaction.symbol.toString());
-            htmlRes += tdMaker(transaction.quantity.toString());
-            htmlRes += tdMaker(transaction.price.toString());
-            htmlRes += tdMaker(transaction.date.toString());
-            htmlRes += tdMaker(transaction.state.toString());
+            htmlRes += tdMaker(transaction.symbol.toString(),"text-center");
+            htmlRes += tdMaker(transaction.quantity.toString(),"text-center");
+            htmlRes += tdMaker(transaction.price.toString(),"text-center");
+            htmlRes += tdMaker(transaction.date.toString(),"text-center");
+            const state = transaction.state.toString();
+            if (state == "pending") {
+                htmlRes += tdMaker(state,"text-center");
+            } else if (state == "success") {
+                htmlRes += tdMaker(state,"text-green-500" + " " + "text-center");
+            } else {
+                htmlRes += tdMaker(state,"text-red-500" + " " + "text-center");
+            }
             htmlRes += "</tr>";
         }
         return { success: true, data: htmlRes }; 
