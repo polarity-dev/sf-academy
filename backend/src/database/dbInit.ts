817a29356a3d511d95c32bd45f670d16df632553
papera_matta <dalpone2005@gmail.com>
2024-09-04 19:05:35 +0200

    updated env variables in dockers and in database init file

--- backend/src/database/dbInit.ts
+++ backend/src/database/dbInit.ts
@@ -1,16 +1,17 @@
 import { Client } from "pg";
+import { env } from "process";
 
 const db = new Client({
-    user: 'myuser',
-    host: 'db',
-    database: 'mydatabase',
-    password: 'mypassword',
-    port: 5432
+    user: env.user ?? 'myuser',
+    host: env.host ?? 'db',
+    database: env.database ?? 'mydatabase',
+    password: env.password ?? 'mypassword',
+    port: (env.port ? Number(env.port) : 5432)
 });
 
 db.connect((err) => {
     if (err) {
-        console.log("Error connecting to the database:",err);
+        console.error("Error connecting to the database:",err);
         process.exit(1);
     } else {
         console.log("Connected successfully to the database");
