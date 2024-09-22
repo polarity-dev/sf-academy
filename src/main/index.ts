18c7944104a2f3a8a876da1aee9eba79ad2442ed
haoranchenn <chen.haoran4@gmail.com>
2024-09-22 11:18:12 +0200

    base api

--- src/main/index.ts
+++ src/main/index.ts
@@ -7,15 +7,62 @@ const server = fastify({ logger: true })
 
 void (async () => {
 
-    server.get('/ping', async (request, reply) => {
-    
-        return 'pong\n'
+    //SSE manager
+    const sseManager = await createSSEManager({
+        httpAdapter: new FastifyHttpAdapter()
     })
+
+    const room = "crypto-list"
+
+    setInterval(async() => {
+        await sseManager.broadcast(room, { data: "8  080" })
+    }, 10000)
+
+
+    //HTML api
     server.get('/', async (request, reply) => {
         const stream = fs.readFileSync("ui/index.html")
         reply.type('text/html').send(stream)
         
     })
+
+    server.get("/crypto-list", async(req, res) => {
+        const sseStream = await sseManager.createSSEStream(res)
+        sseStream.broadcast({ data: "Joining you to test-room" })
+        await sseStream.addToRoom(room)
+        console.log("Successfully joined sseStream")
+    })
+
+    server.get("/queue", async(req, res) => {
+        return "queue"
+    })
+
+    server.get("/transactions", async(req, res) => {
+        return "transactions"
+    })
+
+
+
+
+    //Json api
+    const baseJsonPath = '/api'
+
+    server.get(baseJsonPath+'/crypto', async (request, reply) => {
+    
+        return 'pong\n'
+    })
+
+    server.get(baseJsonPath+'/transactions', async (request, reply) => {
+    
+        return 'pong\n'
+    })
+
+    server.post(baseJsonPath+'/transactions', async (request, reply) => {
+    
+        return 'pong\n'
+    })
+
+
 
     server.listen({ port: 4000, host: '0.0.0.0' }, (err, address) => {
         if (err) {
