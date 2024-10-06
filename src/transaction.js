57522effd7ed681540db15d7a00993ed3914c939
haoranchenn <chen.haoran4@gmail.com>
2024-10-06 15:36:59 +0200

    transaction command implemented

--- src/transaction.js
+++ src/transaction.js
@@ -1,6 +1,5 @@
 const { program } = require('commander');
 const db = require("./main/manager/dbManager");
-const user = require("./main/index/user")
 const cm = require("./main/manager/cryptoManager");
 
 program
@@ -20,47 +19,53 @@ const cryptoManager = new cm.CryptoManager()
 
 void (async () => {
     if(cryptoOpt && typeOpt){
-        let result = db.getCryptoByName(cryptoOpt)
-        if(result.rows.lenght === 0)
-            result = db.getCryptoById(cryptoOpt)
+        if(typeOpt === 'sell' || typeOpt ==='buy'){
+            let result = db.getCryptoByName(cryptoOpt)
+            if(result.rows.lenght === 0)
+                result = db.getCryptoById(cryptoOpt)
 
-        if(result.rows.lenght === 0){
-            console.log('errore nel recupero della crypto')
-        } else {
-            const resultWallet  = await dbManager.getUserWallet(user.id)
-            const wallet = resultWallet.rows
-            const isValid = cryptoManager.checkIfTransactionIsValidWithData(quantityOpt, typeOpt, wallet, user, crypto);
-            
-            if(isValid){
-                const walletIndex = wallet.findIndex((x) => x.cryptoId === crypto.id);
+            if(result.rows.lenght === 0){
+                console.log('errore nel recupero della crypto')
+            } else {
+                const user = db.getUserById(1).rows[0]
+                const crypto = result.rows[0]
+                const resultWallet  = await dbManager.getUserWallet(user.id)
+                const wallet = resultWallet.rows
+                const isValid = cryptoManager.checkIfTransactionIsValidWithData(quantityOpt, typeOpt, wallet, user, crypto);
                 
-                if(typeOpt === 'sell'){
-                    if(walletIndex !== -1) {
-                        wallet[walletIndex].quantity -= quantityOpt;
-                        dbManager.updateUserWallet(wallet[walletIndex]);
-                    }
+                if(isValid){
+                    const walletIndex = wallet.findIndex((x) => x.cryptoId === crypto.id);
                     
-                    user.balance = Math.round((user.balance + (quantityOpt * crypto.price)) * 100) / 100;
+                    if(typeOpt === 'sell'){
+                        if(walletIndex !== -1) {
+                            wallet[walletIndex].quantity -= quantityOpt;
+                            dbManager.updateUserWallet(wallet[walletIndex]);
+                        }
+                        
+                        user.balance = Math.round((user.balance + (quantityOpt * crypto.price)) * 100) / 100;
 
-                    crypto.quantity += quantityOpt;
-                    dbManager.updateCryptoQuantity(crypto);
-                } else { 
+                        crypto.quantity += quantityOpt;
+                        dbManager.updateCryptoQuantity(crypto);
+                    } else { 
 
-                    crypto.quantity -= quantityOpt;
-                    dbManager.updateCryptoQuantity(crypto);
-                
-        
-                    user.balance = Math.round((user.balance - (quantityOpt * crypto.price)) * 100) / 100;
-        
-                    if(walletIndex !== -1) {
-                        wallet[walletIndex].quantity += quantityOpt;
-                        dbManager.updateUserWallet(wallet[walletIndex]);
-                    } else {
-                        dbManager.insertUserWallet(user.id, crypto.id, quantityOpt);
+                        crypto.quantity -= quantityOpt;
+                        dbManager.updateCryptoQuantity(crypto);
+                    
+            
+                        user.balance = Math.round((user.balance - (quantityOpt * crypto.price)) * 100) / 100;
+            
+                        if(walletIndex !== -1) {
+                            wallet[walletIndex].quantity += quantityOpt;
+                            dbManager.updateUserWallet(wallet[walletIndex]);
+                        } else {
+                            dbManager.insertUserWallet(user.id, crypto.id, quantityOpt);
+                        }
                     }
                 }
-            }
-        } 
+            } 
+        }else{
+            console.log('il campo type puo\' essere solo sell o buy') 
+        }
     }else {
         console.log('i campi crypto e type sono obbligatori')
     }
