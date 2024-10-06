439c75a7b389ca4a3058fc31ad99a0b64265fd95
haoranchenn <chen.haoran4@gmail.com>
2024-10-06 16:49:01 +0200

    removed console log

--- src/main/entity/user.ts
+++ src/main/entity/user.ts
@@ -6,7 +6,6 @@ export class User{
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     constructor(row : any){
-        console.log(row.balance)
         this.id = row.id
         this.name = row.name
         this.balance = Number(row.balance)
